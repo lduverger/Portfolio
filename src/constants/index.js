2ea040e2d7d436058d2b6473be4b638de97dd6fc
lduverger <duverger.loic@live.fr>
2024-05-28 21:52:34 +0200

    create file for constants

---
+++ src/constants/index.js
@@ -0,0 +1,120 @@
+const navLinks = [
+  {
+    id: "about",
+    title: "A propos",
+  },
+  {
+    id: "work",
+    title: "Experience",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const projects = [
+  {
+    name: "Booki",
+    image: "",
+    description: "Création d'une page d'accueil pour un site de voyage en utilisant seulement en HTML et CSS, à partir de maquettes Figma. Mise en place de la notion d'interface responsive.",
+    tags: [
+
+    ],
+    code_link: "https://github.com/lduverger/Booki"
+  },
+  {
+    name: "OhMyFood",
+    image: "",
+    description: "Création d'un site pour répertorier les menus d'un restaurant. Apprentissage du préprocesseur SASS, afin de mieux structurer et optimiser le CSS tout en créant des animations",
+    tags: [
+      
+    ],
+    code_link: "https://github.com/lduverger/ohmyfood"
+  },
+  {
+    name: "Portfolio Sophie Bluel",
+    image: "",
+    description: "Création d'une page web dynamique à partir d'une maquette Figma et en utilisant Javascript. Apprentissage de la communication avec une API",
+    tags: [
+      
+    ],
+    code_link: "https://github.com/lduverger/Portfolio-architecte-sophie-bluel"
+  },
+  {
+    name: "Kasa",
+    image: "",
+    description: "Création d'une application de location immobilière en utilisant React. Premier projet avec React à partir d'une maquette Figma.",
+    tags: [
+      
+    ],
+    code_link: "https://github.com/lduverger/Kasa"
+  },
+  {
+    name: "ArgentBank",
+    image: "",
+    description: "Développement Front-end d'une application bancaire en utilisant React et Redux. Il a fallut recréer l'application en React à partir de fichier HTML, mettre en place Redux pour le partage des données entre les composants et mettre une authentification ",
+    tags: [
+      
+    ],
+    code_link: "https://github.com/lduverger/ArgentBank"
+  },
+];
+
+const technos = [
+  {
+    name: "HTML 5",
+    icon: html,
+  },
+  {
+    name: "CSS 3",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: javascript,
+  },
+  {
+    name: "React JS",
+    icon: reactjs,
+  },
+  {
+    name: "Redux Toolkit",
+    icon: redux,
+  },
+  {
+    name: "Vite",
+    icon: vite,
+  }
+  // {
+  //   name: "Node JS",
+  //   icon: nodejs,
+  // },
+  {
+    name: "git",
+    icon: git,
+  },
+];
+
+const experiences = [
+  {
+    title: "experience1",
+    company: "Compagny1", 
+    date: "date date",
+    tasks: [
+      "blabla1",
+      "blabla2",
+    ]
+  },
+  {
+    title: "experience2",
+    company: "Compagny2", 
+    date: "date2 - date 2",
+    tasks: [
+      "blabla1",
+      "blabla2",
+    ]
+  },
+]
+
+export {navLinks, projects, technos, experiences};
