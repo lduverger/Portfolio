55167db3ef43674e6896aab6bfd28746c397024c
lduverger <duverger.loic@live.fr>
2024-05-28 21:51:48 +0200

    add img in assets

---
+++ src/assets/index.js
@@ -0,0 +1,21 @@
+import github from "./github.png"
+
+import css from "./technos/css.png";
+import git from "./technos/git.png";
+import html from "./technos/html.png";
+import javascript from "./technos/javascript.png";
+import nodejs from "./technos/nodejs.png";
+import reactjs from "./technos/reactjs.png";
+import redux from "./technos/redux.png";
+
+
+export {
+    github,
+    css,
+    git,
+    html,
+    javascript,
+    nodejs,
+    reactjs,
+    redux,
+}
